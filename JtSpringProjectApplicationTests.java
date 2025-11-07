700b9ea69c0862b6775b2559d80e8ee69b2b380e
Fahim Imtiaz <90858661+fahimquicklife8@users.noreply.github.com>
2025-11-07 22:32:09 +0000

    Add intentional errors for observability testing
    Introduce errors in the test code for observability agent to catch.

---
+++ JtSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtspringproject.JtSpringProject;
+//Introduce errors in this code for observability agent to catch
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
