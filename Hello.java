bd89817e3a2ed8f3202c0091038e3730fd6e26e5
Fahim Imtiaz <90858661+fahimquicklife8@users.noreply.github.com>
2025-11-14 04:10:05 -0500

    Add Hello.java to print 'Hello, world!'

---
+++ Hello.java
@@ -0,0 +1,10 @@
+// This is a simple Java program that prints "Hello, world!" to the console.
+public class Main {
+
+    // The main method is the entry point for any Java application.
+    public static void main(String[] args) {
+
+        // Print a message to standard output.
+        System.out.println("Hello, world!");
+    }
+}
